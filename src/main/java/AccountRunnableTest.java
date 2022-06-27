08bd0d21e1ac171397f1337a6b046de0e739f612
fengjian2705 <fengjian2705@sina.com>
2022-06-28 00:57:52 +0800

    feat: ReentrantLock；

--- src/main/java/AccountRunnableTest.java
+++ src/main/java/AccountRunnableTest.java
@@ -1,7 +1,8 @@
-
+import java.util.concurrent.locks.ReentrantLock;
 
 public class AccountRunnableTest implements Runnable {
 
+    private ReentrantLock lock = new ReentrantLock();// 准备了一把锁
     private Demo demo = new Demo();
     private int balance;// 用于描述账户的余额
 
@@ -22,29 +23,31 @@ public class AccountRunnableTest implements Runnable {
 
     @Override
     public /*synchronized*/ void run() {
+        // 开始加锁
+        lock.lock();
         System.out.println("线程" + Thread.currentThread().getName() + "已启动");
         // 由源码可知：最终是 account 对象来调用 run 方法，因此当前正在调用的对象就是 account，也就是 this 就是 account
-        synchronized (this) {
+//        synchronized (this) {
 //        synchronized (new Demo()){
-            // 1. 模拟从后台查询账户余额的过程
-            int temp = getBalance();
-            // 2. 模拟取款 200 元的过程
-            if (temp >= 200) {
-                System.out.println("正在出钞，请稍后");
-                temp -= 200;
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("请取走您的钞票 !");
-            } else {
-                System.out.println("余额不足，请核对您的账户余额 !");
+        // 1. 模拟从后台查询账户余额的过程
+        int temp = getBalance();
+        // 2. 模拟取款 200 元的过程
+        if (temp >= 200) {
+            System.out.println("正在出钞，请稍后");
+            temp -= 200;
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            // 3. 模拟将最新的账户余额写入到后台
-            setBalance(temp);
+            System.out.println("请取走您的钞票 !");
+        } else {
+            System.out.println("余额不足，请核对您的账户余额 !");
         }
-
+        // 3. 模拟将最新的账户余额写入到后台
+        setBalance(temp);
+//        }
+        lock.unlock();
     }
 
     public static void main(String[] args) {
