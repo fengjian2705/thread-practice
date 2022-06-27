ca4c1d2beb07a734115343aaf6bb1a30159e9d1d
fengjian2705 <fengjian2705@sina.com>
2022-06-28 00:15:24 +0800

    feat: 同步代码块；

---
+++ src/main/java/AccountRunnableTest.java
@@ -0,0 +1,70 @@
+public class AccountRunnableTest implements Runnable {
+
+    private Demo demo = new Demo();
+    private int balance;// 用于描述账户的余额
+
+    public AccountRunnableTest() {
+    }
+
+    public AccountRunnableTest(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("线程" + Thread.currentThread().getName() + "已启动");
+        synchronized (demo) {
+//        synchronized (new Demo()){
+            // 1. 模拟从后台查询账户余额的过程
+            int temp = getBalance();
+            // 2. 模拟取款 200 元的过程
+            if (temp >= 200) {
+                System.out.println("正在出钞，请稍后");
+                temp -= 200;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("请取走您的钞票 !");
+            } else {
+                System.out.println("余额不足，请核对您的账户余额 !");
+            }
+            // 3. 模拟将最新的账户余额写入到后台
+            setBalance(temp);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        AccountRunnableTest account = new AccountRunnableTest(1000);
+        Thread t1 = new Thread(account);
+        Thread t2 = new Thread(account);
+        t1.start();
+        t2.start();
+
+        System.out.println("主线程开始等待...");
+        try {
+            t1.join();
+//            t2.start();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("最终的账户余额为: " + account.getBalance());// 800
+    }
+}
+
+class Demo {
+
+}
---
+++ src/main/java/AccountThreadTest.java
@@ -0,0 +1,65 @@
+public class AccountThreadTest extends Thread {
+
+    private static Demo demo = new Demo();
+    private int balance;// 用于描述账户的余额
+
+    public AccountThreadTest() {
+    }
+
+    public AccountThreadTest(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("线程" + Thread.currentThread().getName() + "已启动");
+        synchronized (demo) {
+//        synchronized (new Demo()){
+            // 1. 模拟从后台查询账户余额的过程
+            int temp = getBalance();
+            // 2. 模拟取款 200 元的过程
+            if (temp >= 200) {
+                System.out.println("正在出钞，请稍后");
+                temp -= 200;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("请取走您的钞票 !");
+            } else {
+                System.out.println("余额不足，请核对您的账户余额 !");
+            }
+            // 3. 模拟将最新的账户余额写入到后台
+            setBalance(temp);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        AccountThreadTest att1 = new AccountThreadTest(1000);
+        att1.start();
+
+        AccountThreadTest att2 = new AccountThreadTest(1000);
+        att2.start();
+
+        System.out.println("主线程开始等待...");
+
+        try {
+            att1.join();
+            att2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("最终的账户余额为: " + att1.getBalance());// 800
+    }
+}
